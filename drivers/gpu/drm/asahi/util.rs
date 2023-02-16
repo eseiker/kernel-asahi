5eaf2c816f0b7764692f893e22542e44f9b45331
Asahi Lina <lina@asahilina.net>
2023-02-17 00:31:51 +0900

    drm/asahi: Add the Asahi driver for Apple AGX GPUs
    Signed-off-by: Asahi Lina <lina@asahilina.net>

---
+++ drivers/gpu/drm/asahi/util.rs
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+
+//! Miscellaneous utility functions
+
+use core::ops::{Add, BitAnd, Div, Not, Sub};
+
+/// Aligns an integer type to a power of two.
+pub(crate) fn align<T>(a: T, b: T) -> T
+where
+    T: Copy
+        + Default
+        + BitAnd<Output = T>
+        + Not<Output = T>
+        + Add<Output = T>
+        + Sub<Output = T>
+        + Div<Output = T>
+        + core::cmp::PartialEq,
+{
+    let def: T = Default::default();
+    #[allow(clippy::eq_op)]
+    let one: T = !def / !def;
+
+    assert!((b & (b - one)) == def);
+
+    (a + b - one) & !(b - one)
+}
