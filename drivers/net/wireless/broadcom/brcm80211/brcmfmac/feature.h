Trijal08/kernel-mainline

wifi: brcmfmac: Implement event_msgs_ext
This extended command supports bit set/clear operations, but we just use
it like the old full mask set command.

Signed-off-by: Hector Martin <marcan@marcan.st>
(cherry picked from commit 09b3b340856d6361a3156b5a04d18ab9742619be)

drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.h
@@ -60,7 +60,8 @@
 	BRCMF_FEAT_DEF(SCAN_V3) \
 	BRCMF_FEAT_DEF(PMKID_V2) \
 	BRCMF_FEAT_DEF(PMKID_V3) \
-	BRCMF_FEAT_DEF(SAE_EXT)
+	BRCMF_FEAT_DEF(SAE_EXT) \
+	BRCMF_FEAT_DEF(EVENT_MSGS_EXT) \
 
 /*
  * Quirks:

eseiker/kernel-asahi

drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.h
@@ -58,7 +58,8 @@
 	BRCMF_FEAT_DEF(SCAN_V2) \
 	BRCMF_FEAT_DEF(SCAN_V3) \
 	BRCMF_FEAT_DEF(PMKID_V2) \
-	BRCMF_FEAT_DEF(PMKID_V3)
+	BRCMF_FEAT_DEF(PMKID_V3) \
+	BRCMF_FEAT_DEF(EVENT_MSGS_EXT) \
 
 /*
  * Quirks: