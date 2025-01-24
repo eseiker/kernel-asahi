06dd0954947bb0395dedfa704fdf38860a697794
Asahi Lina <lina@asahilina.net>
2025-01-25 05:50:58 +0900

    drm/asahi: workqueue: Defer freeing the last completed work item
    Maybe helps with firmware crashes?
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/asahi/workqueue.rs
+++ drivers/gpu/drm/asahi/workqueue.rs
@@ -255,6 +255,7 @@ struct WorkQueueInner {
     size: u32,
     wptr: u32,
     pending: KVec<Pin<KBox<SubmittedWorkContainer>>>,
+    last_completed_work: Option<Pin<KBox<SubmittedWorkContainer>>>,
     last_token: Option<event::Token>,
     pending_jobs: usize,
     last_submitted: Option<event::EventValue>,
@@ -736,6 +737,7 @@ impl WorkQueue::ver {
             size,
             wptr: 0,
             pending: KVec::new(),
+            last_completed_work: None,
             last_token: None,
             event: None,
             priority,
@@ -929,6 +931,8 @@ impl WorkQueue for WorkQueue::ver {
         let last_wptr = inner.pending[completed_commands - 1].inner.wptr();
         let pipe_type = inner.pipe_type;
 
+        let mut last_cmd = inner.last_completed_work.take();
+
         for mut cmd in inner.pending.drain(..completed_commands) {
             mod_pr_debug!(
                 "WorkQueue({:?}): Queueing command @ {:?} for cleanup\n",
@@ -936,8 +940,12 @@ impl WorkQueue for WorkQueue::ver {
                 cmd.inner.gpu_va()
             );
             cmd.as_mut().inner_mut().complete();
-            workqueue::system().enqueue(cmd);
+            if let Some(last_cmd) = last_cmd.replace(cmd) {
+                workqueue::system().enqueue(last_cmd);
+            }
         }
+
+        inner.last_completed_work = last_cmd;
 
         mod_pr_debug!(
             "WorkQueue({:?}): Completed {} commands, left pending {}, ls {:#x?}, lc {:#x?}\n",
@@ -1032,6 +1040,15 @@ impl WorkQueue for WorkQueue::ver {
         for mut cmd in cmds {
             cmd.as_mut().inner_mut().mark_error(error);
             cmd.as_mut().inner_mut().complete();
+        }
+    }
+}
+
+#[versions(AGX)]
+impl Drop for WorkQueueInner::ver {
+    fn drop(&mut self) {
+        if let Some(last_cmd) = self.last_completed_work.take() {
+            workqueue::system().enqueue(last_cmd);
         }
     }
 }
