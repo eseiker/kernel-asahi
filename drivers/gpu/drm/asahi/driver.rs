ed86b09b4d731623a68cd018e07a01de8922ca7e
Janne Grunau <j@jannau.net>
2025-02-26 23:36:09 +0100

    drm/asahi: driver: Port to dma_set_mask_and_coherent() from ml
    Expected to change / move to device.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/asahi/driver.rs
+++ drivers/gpu/drm/asahi/driver.rs
@@ -8,6 +8,8 @@ use kernel::{
 
 use crate::{debug, file, gem, gpu, hw, regs};
 
+use kernel::dma::dma_set_mask_and_coherent;
+use kernel::error::to_result;
 use kernel::macros::vtable;
 use kernel::types::ARef;
 
@@ -145,7 +147,10 @@ impl platform::Driver for AsahiDriver {
 
         let cfg = info.ok_or(ENODEV)?;
 
-        pdev.set_dma_masks((1 << cfg.uat_oas) - 1)?;
+        to_result(dma_set_mask_and_coherent(
+            pdev.as_ref(),
+            (1 << cfg.uat_oas) - 1,
+        ))?;
 
         let res = regs::Resources::new(pdev)?;
 
