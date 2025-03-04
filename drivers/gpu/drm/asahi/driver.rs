b69c8023aa5bea0cd597819a696af48b0147c14c
Janne Grunau <j@jannau.net>
2025-03-04 23:26:04 +0100

    drm/asahi: Add AsahiData to the platform device drvdata
    The newest DRM rust bindings make the device data unavailable via
    drm::device::Device<T> to decouple DRM device creation from the device
    data as this is required planned driver like Tyr. On the surface it is
    required for asahi as well but it should be possible to avoid this.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/asahi/driver.rs
+++ drivers/gpu/drm/asahi/driver.rs
@@ -31,6 +31,7 @@ pub(crate) struct AsahiData {
 
 pub(crate) struct AsahiDriver {
     _reg: drm::drv::Registration<Self>,
+    pub(crate) data: Arc<AsahiData>,
 }
 
 /// Convenience type alias for the DRM device type for this driver.
@@ -202,6 +203,6 @@ impl platform::Driver for AsahiDriver {
 
         let reg = drm::drv::Registration::new(drm, data.clone(), 0)?;
 
-        Ok(KBox::new(Self { _reg: reg }, GFP_KERNEL)?.into())
+        Ok(KBox::new(Self { _reg: reg, data }, GFP_KERNEL)?.into())
     }
 }
